6d4863dbc8f7b9e827908f6967d47bbf09e39e89
Arpit <arpit_jain@live.com>
2018-01-19 16:54:36 +0530

    Component Part II

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,29 +2,23 @@ import { NgModule } from '@angular/core';
 
 import {
   MatButtonModule,
-  MatMenuModule,
   MatToolbarModule,
-  MatIconModule,
   MatCardModule,
-  MatListModule,
+  MatGridListModule
 } from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule,
-    MatMenuModule,
     MatToolbarModule,
-    MatIconModule,
     MatCardModule,
-    MatListModule,
+    MatGridListModule
   ],
   exports: [
     MatButtonModule,
-    MatMenuModule,
     MatToolbarModule,
-    MatIconModule,
     MatCardModule,
-    MatListModule,
+    MatGridListModule
   ]
 })
 export class MaterialModule {}
